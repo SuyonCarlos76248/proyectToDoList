fc562b622467969edef69d9e67b1239d34f12600
SuyonCarlos76248 <81585386+SuyonCarlos76248@users.noreply.github.com>
2022-05-03 10:59:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
 /*var*/
 
-
-
 /*functions*/
 
 function AddTask()
 {
-    let input = document.getElementById('txtTask'); 
-    let list = document.getElementById('lstTask');
-    let li = document.createElement('li');	
+	debugger;  // <-- the debugger stops here
+	let input = document.getElementById('txtTask'); 
+	let list = document.getElementById('lstTask');
+	let li = document.createElement('li');				
+	let btnTask = document.getElementById('btnTask');
+	
+	let size = document.getElementsByTagName('li').length;	
 	let space = "";
 	let tablaCompleta = false;
-	let size = document.getElementsByTagName('li').length;	
-	let btnTask = document.getElementById('btnTask');
 
-	if ( input.value == space)
+	if ( (input.value) == space)
 	{
 		window.alert("Ingresa tarea por favor.");	
 	}
@@ -48,13 +48,16 @@ function DeleteTask()
     let input = document.getElementById('txtTask');
 	let space = "";
 	let list = document.getElementById('lstTask');	
-	let size =  document.getElementsByTagName('li').length
-	let selected_item = document.getElementById(input.value);    
-
-	if (size = 0)
+	let size =  document.getElementsByTagName('li').length;
+	let selected_item = document.getElementById(input.value);  
+	let btnDelete = document.getElementById('btnDelete');
+	
+	if (size == 0)
 	{
 		/* No hay elementos en la lista*/
+		btnDelete.disabled = true;
 		window.alert("No hay tareas a Borrar.");
+		
 	}
 	else
 	{
@@ -65,16 +68,22 @@ function DeleteTask()
 		else
 		{
 			/* se debe validar que el input sea una tarea que se encuentra en la lista */
-			list.removeChild(selected_item);
-			/*  se debe validar que si el tamaño de la lista = 0 si es asi btnDelete.disabled = true.*/
-			window.alert("Se borro la tarea exitosamente.");	
-				
+			list.removeChild(selected_item);			
+			window.alert("Se borro la tarea exitosamente.");
+			if (size <=3 /* 28 */ )
+			{
+				btnTask.disabled = false;
+			}
 		}
 	}
 	
 }
 
-
-/*
-  se de debe desabilitar el btn delete task cuando la lista este vacia 
-*/
+function SearchTask()
+{
+	let size = document.getElementsByTagName('li').length;	
+	for (var i = 0; i < size; i++)
+	{
+		/* recorrer los hijos de list y verificar si es igual al input que se desea eliminar */
+	}
+}
